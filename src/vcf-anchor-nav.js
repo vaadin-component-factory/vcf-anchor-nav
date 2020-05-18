a25bc3c49099fcbe4895184486cc9e642d50061f
Nii Yeboah <nii@vaadin.com>
2020-05-18 21:06:29 +0300

    Update src version: 0.1.0 -> 0.1.1

--- src/vcf-anchor-nav.js
+++ src/vcf-anchor-nav.js
@@ -69,7 +69,7 @@ class VcfAnchorNav extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '0.1.0';
+    return '0.1.1';
   }
 
   static get properties() {
