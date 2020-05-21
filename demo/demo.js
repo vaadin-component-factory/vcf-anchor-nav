762991d11cfac1bdc9d756c034d5969fe6f09927
Nii Yeboah <nii@vaadin.com>
2020-05-21 04:18:52 +0300

    Update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,6 @@
 import '@polymer/iron-demo-helpers/demo-pages-shared-styles';
 import '@polymer/iron-demo-helpers/demo-snippet';
 import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-lumo-styles/icons';
+import '@vaadin/vaadin-lumo-styles/typography';
 import '../theme/lumo/vcf-anchor-nav';
