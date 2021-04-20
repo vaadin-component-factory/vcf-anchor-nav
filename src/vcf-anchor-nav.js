fb2fb95080cefcfa939279e0c1d4ce78ceb9e1b3
Nii Yeboah <nii@vaadin.com>
2021-04-20 16:52:57 +0300

    v1.2.0-beta.2

--- src/vcf-anchor-nav.js
+++ src/vcf-anchor-nav.js
@@ -139,7 +139,7 @@ export class AnchorNavElement extends ElementMixin(ThemableMixin(PolymerElement)
   }
 
   static get version() {
-    return '1.2.0-beta.1';
+    return '1.2.0-beta.2';
   }
 
   static get properties() {
