e0df0b9bb5173163d9c8d023fd39f88034fce544
Nii Yeboah <nii@vaadin.com>
2022-06-28 23:23:28 +0300

    v23.1.0

--- src/vcf-anchor-nav.js
+++ src/vcf-anchor-nav.js
@@ -148,7 +148,7 @@ export class AnchorNavElement extends ElementMixin(ThemableMixin(PolymerElement)
   }
 
   static get version() {
-    return '23.0.1';
+    return '23.1.0';
   }
 
   static get properties() {
