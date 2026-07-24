d3a82e8c98096bc110c64ac9cbaa26058a6bc425
Paola De Bartolo <paola.debartolo@gmail.com>
2026-07-24 11:43:55 -0300

    build: update component version to 4.0.1

--- src/vcf-anchor-nav.js
+++ src/vcf-anchor-nav.js
@@ -161,7 +161,7 @@ export class AnchorNavElement extends ThemeDetectionMixin(ElementMixin(LitElemen
   }
 
   static get version() {
-    return '4.0.0';
+    return '4.0.1';
   }
 
   static get properties() {
